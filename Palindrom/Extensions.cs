527c60258eed7ebcd817e9dbcf86bcf3148738b6
makskayun <20Nebeyvspinu00>
2022-06-03 19:46:08 +0300

    init

---
+++ Palindrom/Extensions.cs
@@ -0,0 +1,58 @@
+﻿namespace Palindrom;
+
+public static class Extensions
+{
+    public static IEnumerable<int> GetReverseNumbersCollection(this string[]? arr, Predicate<int> condition)
+    {
+        List<int> result = new List<int>();
+        Array.Reverse(arr);
+        arr.Select(int.Parse).ToList().ForEach(e =>
+        {
+            if (condition(e))
+            {
+                result.Add(e);
+            }
+        });
+        return result;
+    }
+
+    public static string GetFirstOrEmptyIf(this string[]? arr, Predicate<string> condition)
+    {
+        foreach (var name in arr)
+        {
+            if (condition(name))
+            {
+                return name;
+            }
+        }
+        return string.Empty;
+    }
+
+    public static IEnumerable<int> ReshapeAndSort(this IEnumerable<int>? collect, Predicate<int> condition)
+    {
+        List<int> result = new List<int>();
+        collect.ToList().ForEach(e =>
+        {
+            if (condition(e))
+            {
+                result.Add(e);
+            }
+        });
+        result.Sort();
+        return result;
+    }
+
+    public static List<string> CheckStringsAndSort(this List<string> collection, Predicate<string> condition)
+    {
+        List<string> result = new List<string>();
+        collection.ForEach(s =>
+        {
+            if (condition(s))
+            {
+                result.Add(s);
+            }
+        });
+        result.Sort();
+        return result;
+    }
+}
---
+++ Palindrom/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+Console.ReadLine().Split(' ')
+    .OrderBy(x => x.Length).ToList().ForEach(s =>
+    {
+        if (s.Length <= num)
+        {
+            Console.Write(s);
+            Environment.Exit(0);
+        }
+    });
